92d517e48f8ee5c78568ab398a696be30bb3c2ec
abdulwe5 <147302247+abdulwe5@users.noreply.github.com>
2024-03-30 19:02:17 +0530

    Add files via upload

---
+++ ATS/demo/src/main/java/ASimulatorSystem/Conn.java
@@ -0,0 +1,23 @@
+package ASimulatorSystem;
+
+import java.sql.*;
+
+public class Conn {
+    Connection c;
+    Statement s;
+
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "Abdulnasim@123");
+            s = c.createStatement();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public Connection getConnection() {
+        return c;
+    }
+}
